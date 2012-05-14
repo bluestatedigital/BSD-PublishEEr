8a45f4c3772a9af1d53a13d0463b52aacd65201e
Douglas Back <douglas@bluestatedigital.com>
2012-05-14 09:45:28 -0400

    minified var names

--- bsd_publisheer/javascript/fix-menu.js
+++ bsd_publisheer/javascript/fix-menu.js
@@ -4,6 +4,7 @@ $channels = $publishMenu.children("li").addClass("channels").detach();
 $("#publish-menu").prepend("<li id=\"listjs-search-li\"><label id=\"listjs-search-label\" for=\"list-js-search\">Channel Search<input id=\"listjs-search\" type=\"text\" class=\"search\" placeholder=\"Channel Name\"/></label></li>");
 $publishMenu.append("<li id=\"channel-list\"><ul class=\"listjs\"></ul></li>");
 $channels.appendTo("#channel-list ul.listjs").children("a").addClass("channel-name");
-var options = { valueNames: ["channel-name"], listClass: "listjs" };
-var channelList = new List("publish-menu", options);
+var o = { valueNames: ["channel-name"], listClass: "listjs" };
+var c = new List("publish-menu", o);
 })(jQuery);
+
